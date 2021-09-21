df1b7ab1f69caf0300499adf5f391ee24903c191
alwxkxk <592364224@qq.com>
2021-09-21 09:35:39 +0800

    feat: react hook加载three.js场景。

--- web-client/src/App.js
+++ web-client/src/App.js
@@ -1,19 +1,20 @@
 import { Tabs  } from 'antd';
 import GreetTest from './components/GreetTest';
+import ModelViewer from './pages/model-viewer';
 import 'antd/dist/antd.css';
 import './App.css';
 
 const { TabPane } = Tabs;
 function App() {
   return (
-    <Tabs defaultActiveKey="3" type="card">
+    <Tabs defaultActiveKey="2" type="card">
       <TabPane tab="Tab 1" key="1">
         Content of Tab Pane 1
       </TabPane>
-      <TabPane tab="Tab 2" key="2">
-        Content of Tab Pane 2
+      <TabPane tab="3D模型分析页" key="2">
+        <ModelViewer/>
       </TabPane>
-      <TabPane tab="功能测试页" key="3">
+      <TabPane tab="后端接口测试页" key="3">
         <GreetTest/>
       </TabPane>
     </Tabs>
---
+++ web-client/src/pages/model-viewer/index.js
@@ -0,0 +1,10 @@
+import ModelScene from './components/ModelScene.js'
+function ModelViewer(params) {
+    return (
+        <div>
+            <ModelScene/>
+        </div>
+    )
+}
+
+export default ModelViewer
