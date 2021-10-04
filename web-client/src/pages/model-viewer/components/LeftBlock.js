55105f1bd10ac3f9b787a8c1daf2246cbf70c7ed
suxiqiang <suxiqiang@gzrsic.com>
2021-10-04 13:04:59 +0800

    feat: 添加模型大小 摄像头位置信息。

--- web-client/src/pages/model-viewer/components/LeftBlock.js
+++ web-client/src/pages/model-viewer/components/LeftBlock.js
@@ -4,22 +4,35 @@ import { useState } from 'react'
 
 /**
  * @param  {Function} params.uploadChange 选择文件后的触发函数
+ * @param  {Object} params.viewInfo 场景信息
+ * @param  {Object} params.cameraPositionInfo 摄像头位置信息
  */
 function LeftBlock (params) {
   const [fileNameList, setFileNameList] = useState([])
-  const [fileSize, setfileSize] = useState(0)
+  const [fileSize, setFileSize] = useState(0)
+
+  let boxInfo = null
+  let cameraPositionInfo = null
+
+  if (params.viewInfo && params.viewInfo.box) {
+    boxInfo = params.viewInfo.box
+  }
+
+  if (params.cameraPositionInfo) {
+    cameraPositionInfo = params.cameraPositionInfo
+  }
   function uploadChangeWrap (event) {
     // 读取文件信息
     const target = event.target
     const files = target.files
-    console.log('uploadChangeWrap', event, files)
+    // console.log('uploadChangeWrap', event, files)
     let fileSize = 0
     const fileNameList = Array.from(files).map(file => {
       fileSize += file.size
       return file.name
     })
     setFileNameList(fileNameList)
-    setfileSize(fileSize)
+    setFileSize(fileSize)
 
     if (params.uploadChange) {
       params.uploadChange(event)
@@ -37,6 +50,27 @@ function LeftBlock (params) {
         {
           fileSize !== 0 &&
           <div>文件大小：{(fileSize / 1024).toFixed(0) } KB</div>
+        }
+        {
+          boxInfo &&
+          <div>
+            <div>当前模型尺寸</div>
+            <div>X：{(boxInfo.max.x - boxInfo.min.x).toFixed(3)}({(boxInfo.min.x).toFixed(3)}~{(boxInfo.max.x).toFixed(3)})</div>
+            <div>Y：{(boxInfo.max.y - boxInfo.min.y).toFixed(3)}({(boxInfo.min.y).toFixed(3)}~{(boxInfo.max.y).toFixed(3)})</div>
+            <div>Z：{(boxInfo.max.z - boxInfo.min.z).toFixed(3)}({(boxInfo.min.z).toFixed(3)}~{(boxInfo.max.z).toFixed(3)})</div>
+          </div>
+
+        }
+
+      {
+          cameraPositionInfo &&
+          <div>
+            <div>当前摄像头位置</div>
+            <div>X：{cameraPositionInfo.x.toFixed(3)}</div>
+            <div>Y：{cameraPositionInfo.y.toFixed(3)}</div>
+            <div>Z：{cameraPositionInfo.z.toFixed(3)}</div>
+          </div>
+
         }
 
       </div>
--- web-client/src/pages/model-viewer/components/ModelScene.js
+++ web-client/src/pages/model-viewer/components/ModelScene.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import {
@@ -14,23 +14,32 @@ import './ModelScene.css'
 
 function ModelScene (params) {
   const ref = useRef()
-  const scene = new THREE.Scene()
-  // TODO:背景色可调整
-  scene.background = new THREE.Color('#ccc')
-  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-  const renderer = new THREE.WebGLRenderer()
-  const orbit = new OrbitControls(camera, renderer.domElement)
-
-  orbit.autoRotate = true
-  orbit.autoRotateSpeed = 2
+  const [viewInfo, setViewInfo] = useState({})
+  const [scene, setScene] = useState()
+  const [camera, setCamera] = useState()
+  const [orbit, setOrbit] = useState()
+  const [cameraPositionInfo, setCameraPositionInfo] = useState()
 
   useEffect(() => {
+    const scene = new THREE.Scene()
+    setScene(scene)
+    // TODO:背景色可调整
+    scene.background = new THREE.Color('#ccc')
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+    setCamera(camera)
+    const renderer = new THREE.WebGLRenderer()
+    const orbit = new OrbitControls(camera, renderer.domElement)
+    setOrbit(orbit)
+
+    orbit.autoRotate = true
+    orbit.autoRotateSpeed = 2
+
     renderer.setSize(window.innerWidth / 2, window.innerHeight / 2)
     ref.current.appendChild(renderer.domElement)
-    renderer.domElement.style = 'margin:auto;'
 
     loadGlb(scene, '/model/computer.glb').then(() => {
-      frameTargetView(scene, camera, orbit)
+      const viewInfo = frameTargetView(scene, camera, orbit)
+      setViewInfo(viewInfo)
     })
 
     camera.position.z = 5
@@ -39,43 +48,44 @@ function ModelScene (params) {
     const animate = function () {
       animationFrameFlag = requestAnimationFrame(animate)
       orbit.update()
-
+      setCameraPositionInfo({ ...camera.position })
       renderer.render(scene, camera)
     }
 
     animate()
     return () => {
-      // TODO：释放资源
+      // 释放资源
       cancelAnimationFrame(animationFrameFlag)
       renderer.domElement.remove()
     }
-  })
+  }, []) // 任何变化都不会再触发调用，即只在第一次更新
 
   function uploadChange (event) {
     const target = event.target
     const files = target.files
     if (files.length === 1) {
-      // TODO:URL.revokeObjectURL()释放资源
       const url = window.URL.createObjectURL(files[0])
       // console.log('change to url', url)
 
       if (files[0].name.includes('.glb')) {
         loadGlb(scene, url).then(() => {
-          frameTargetView(scene, camera, orbit)
+          // frameTargetView(scene, camera, orbit)
           window.URL.revokeObjectURL(url)
+          const viewInfo = frameTargetView(scene, camera, orbit)
+          setViewInfo(viewInfo)
         })
       } else {
         message.warn('单个模型文件应为glb格式。')
       }
       // TODO: 支持gltf多文件
     }
-    console.log('uploadChange', target, files)
+    // console.log('uploadChange', target, files)
   }
 
   return (
 
     <div className="flex">
-      <LeftBlock uploadChange={uploadChange} />
+      <LeftBlock uploadChange={uploadChange} viewInfo={viewInfo} cameraPositionInfo={cameraPositionInfo}/>
       {/* 中间插入3D场景 */}
       <div ref={ref} className="flex-1"></div>
       <RightBlock/>
