8fcc79229bf4e0fee424b64261d35e26f2d64829
alwxkxk <592364224@qq.com>
2022-02-27 21:06:44 +0800

    feat: 添加websocket.

--- server/src/main/java/com/alwxkxk/server/controller/GreetingController.java
+++ server/src/main/java/com/alwxkxk/server/controller/GreetingController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ *  http 路由测试代码 greeting
+ */
 @RestController
 public class GreetingController {
     private static final String template = "Hello, %s!";
